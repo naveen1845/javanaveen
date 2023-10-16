d1217bf5197d99ae85c4533df498161898d68555
naveen1845 <148039867+naveen1845@users.noreply.github.com>
2023-10-16 07:39:43 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treats all JS code as neve version
+
+// alert(2+2+4)  // we are using nodejs, not browser
+
+// code redability is very important
+
+let name = "naveen"
+let age = 18
+let isLoggedIn = true
+
+// number => 2 to the power of 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol 
+
+
+// object
+
+console.log(typeof undefined)  // undefined
+console.log(typeof null)    // object
