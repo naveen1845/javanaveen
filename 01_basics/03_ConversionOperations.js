b7df2783b3bd7e778ab4f8a777ba7b053dda18a5
naveen1845 <148039867+naveen1845@users.noreply.github.com>
2023-10-16 13:55:26 +0000

    conversion of data types

---
+++ 01_basics/03_ConversionOperations.js
@@ -0,0 +1,24 @@
+let score = null
+
+console.log(typeof score)
+
+let ValueInNumber= Number(score)  //converts the calue stored in a variable to number 
+
+console.log(typeof ValueInNumber);
+
+console.log(ValueInNumber)
+
+// "33" => 33
+// "33anc" => NaN
+// null => 0
+// true => 1   false => 0
+
+let IsLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(IsLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true  0 => false
+// "" => true  "naveen" => true
+// 25 => true
+
