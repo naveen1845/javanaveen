36b68a189a8fd1711671e545a0c08e77f042d678
naveen1845 <148039867+naveen1845@users.noreply.github.com>
2023-10-17 04:56:42 +0000

    stacks and heap

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -35,3 +35,31 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+//****************************************
+
+
+// stack (primitive), heap (non-primitive)
+
+
+// stack => copy
+let name1 = "naveen"
+let name2 = name1
+
+name2 = "ronaldo"
+
+console.log(name1,"   ", name2)
+
+
+// heap => reference
+let user1={
+    email:"gg@gmail.com",
+    upi:"gg@ybl"
+
+}
+
+let user2 = user1
+
+user2.email = "vk@gmail.com"
+
+console.log(user1.email,"   ",user2.email);
